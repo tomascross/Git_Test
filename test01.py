d50efa5cf1987b5782c89729eea392babee6dce0
Tomas Sedlacek <tomas.sedlacek@crossmasters.com>
2024-09-03 13:42:06 +0200

    fix(added descriptions)

--- test01.py
+++ test01.py
@@ -1,9 +1,10 @@
+#test
 def add_two_integeres(number1,number2):
     return number1 + number2
-
+#test
 def substraction(number1,number2):
     return number1 - number2
 
-
+#test
 def division(number1,number2):
     return number1 / number2
